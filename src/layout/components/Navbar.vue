7a8c4bfb230db66ab71280543680dbe4371c0254
Mr.Hou <1492642409@qq.com>
2022-09-24 15:20:54 +0800

    自定义指令-解决异常图片情况

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -10,7 +10,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="staffPhoto" class="user-avatar">
+          <img v-imagerror="defaultImg" :src="staffPhoto" class="user-avatar">
           <span class="name">{{ name }}</span>
           <i class="el-icon-caret-bottom" style="color:#fff" />
         </div>
@@ -41,6 +41,12 @@ export default {
   components: {
     // Breadcrumb,
     Hamburger
+  },
+  data() {
+    return {
+      // 在js中使用图片, 需要将图片路径变为变量
+      defaultImg: require('@/assets/common/head.jpg')
+    }
   },
   computed: {
     ...mapGetters([
--- src/main.js
+++ src/main.js
@@ -15,14 +15,11 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
+import * as directives from '@/directives'
+Object.keys(directives).forEach(item => {
+  Vue.directive(item, directives[item])
+  // console.log(directives[item])
+})
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
