9de1220532a5f4ba13d0dcec90099726e4710ce8
Mr.Hou <1492642409@qq.com>
2022-09-28 19:10:10 +0800

    将树形的操作内容单独抽提成组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,37 +1,38 @@
 <template>
-  <div class="app-container">
-    <el-card class="tree-card">
-      <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-        <el-col>
-          <span>人力资源后台管理系统——组织架构</span>
-        </el-col>
-        <el-col :span="4">
-          <el-row type="flex" justify="end">
-            <el-col>负责人</el-col>
-            <el-col>
-              <el-dropdown>
-                <span>操作<i class="el-icon-arrow-down" /></span>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </el-col>
-          </el-row>
-        </el-col>
-      </el-row>
-    </el-card>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card class="tree-card">
+        <tree-tools :tree-node="company" :is-root="true" />
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
+        </el-tree>
+      </el-card>
+    </div>
   </div>
 </template>
-
 <script>
+import treeTools from './components/tree-tools.vue'
 export default {
-
+  components: { treeTools },
+  data() {
+    return {
+      defaultProps: {
+        label: 'name' // label默认的值是:'label'
+        // children: 'children' //这个可以省略，children默认的就是'children'
+      },
+      departs: [
+        { name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }
+      ],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }
+    }
+  }
 }
 </script>
-
 <style scoped>
-  .tree-card {
-    padding: 30px  140px;
-    font-size:14px;
-  }
-  </style>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
