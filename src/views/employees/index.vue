0fe5d1476a624fbd67e2005eeb8e04de74d1f5f8
Mr.Hou <1492642409@qq.com>
2022-10-13 15:44:48 +0800

    实现将员工数据导出功能

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -5,7 +5,7 @@
         <span slot="before">共{{ page.total }}条记录</span>
         <template #after>
           <el-button size="small" type="warning" @click="$router.push('/import')">导入</el-button>
-          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="danger" @click="exportData">导出</el-button>
           <el-button size="small" type="primary" @click="showDialog = true">新增员工</el-button>
         </template>
       </page-tools>
@@ -55,7 +55,8 @@
 import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees'
 import AddEmployee from './components/add-employee'
-console.log(EmployeeEnum)
+import { formatDate } from '@/filters'
+// console.log(EmployeeEnum)
 export default {
   components: {
     AddEmployee
@@ -104,6 +105,57 @@ export default {
       } catch (error) {
         // console.log(error)
       }
+    },
+    exportData() {
+      //  做操作
+      // 表头对应关系
+      const headers = {
+        '姓名': 'username',
+        '手机号': 'mobile',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+      // 懒加载
+      import('@/vendor/Export2Excel').then(async excel => {
+        // page: this.page.total 的目的是要一次性把全部的员工列表查询出来
+        const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        // [["张三", "13811111111"，"2018","1", "2018", "10002"],["李思", "13911111111"，"2018","1", "2018", "10002"],...]
+        const data = this.formatJson(headers, rows)
+        const multiHeader = [['姓名', '主要信息', '', '', '', '', '部门']]
+        const merges = ['A1:A2', 'B1:F1', 'G1:G2']
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data,
+          filename: '员工信息表',
+          autoWidth: true,
+          multiHeader, // 复杂表头
+          merges // 合并选项
+        })
+      })
+    },
+    // 该方法负责将数组转化成二维数组
+    formatJson(headers, rows) {
+      return rows.map(item => {
+        // item是一个对象  { mobile: 132111,username: '张三'  }
+        // ["手机号", "姓名", "入职日期" 。。]
+        return Object.keys(headers).map(key => {
+          // 需要判断 字段
+          if (headers[key] === 'timeOfEntry' || headers[key] === 'correctionTime') {
+            // 格式化日期
+            return formatDate(item[headers[key]])
+          } else if (headers[key] === 'formOfEmployment') {
+            const obj = EmployeeEnum.hireType.find(obj => obj.id === item[headers[key]])
+            return obj ? obj.value : '未知'
+          }
+          return item[headers[key]]
+        })
+        // ["132", '张三’， ‘’，‘’，‘’d]
+      })
+      // return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
+      // 需要处理时间格式问题
     }
   }
 
