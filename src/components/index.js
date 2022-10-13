b5376f1063c75aaf252d89d82fa0bb65d150bba1
Mr.Hou <1492642409@qq.com>
2022-10-13 14:56:31 +0800

    实现员工的导入

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import PageTools from './PageTools'
+import UploadExcel from './UploadExcel'
 
 export default {
   install(Vue) {
     // 这里填充的逻辑会被执行
-    Vue.component('PageTools', PageTools)
+    Vue.component('PageTools', PageTools) // 注册工具栏组件
+    Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
   }
 }
 
