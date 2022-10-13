4bd3ca29697b84f80f970503964968965fd65bc7
Mr.Hou <1492642409@qq.com>
2022-10-13 16:44:33 +0800

    实现个人信息的岗位信息的读取和校验，保存

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,5 +1,10 @@
 import request from '@/utils/request'
 
+export function getEmployeeSimple() {
+  return request({
+    url: '/sys/user/simple'
+  })
+}
 /**
  * 获取员工的综合列表数据
  * ***/
@@ -41,6 +46,59 @@ export function importEmployee(data) {
   return request({
     url: '/sys/user/batch',
     method: 'post',
+    data
+  })
+}
+
+/** *
+ *
+ * 保存员工的基本信息
+ * **/
+export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
+
+/** *
+ *  读取用户详情的基础信息
+ * **/
+export function getPersonalDetail(id) {
+  return request({
+    url: `/employees/${id}/personalInfo`
+  })
+}
+
+/** *
+ *  更新用户详情的基础信息
+ * **/
+export function updatePersonal(data) {
+  return request({
+    url: `/employees/${data.userId}/personalInfo`,
+    method: 'put',
+    data
+  })
+}
+
+/** **
+ * 获取用户的岗位信息
+ *
+ * ****/
+export function getJobDetail(id) {
+  return request({
+    url: `/employees/${id}/jobs`
+  })
+}
+
+/**
+ * 保存岗位信息
+ * ****/
+export function updateJob(data) {
+  return request({
+    url: `/employees/${data.userId}/jobs`,
+    method: 'put',
     data
   })
 }
--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -9,6 +9,14 @@ export default {
       path: '',
       component: () => import('@/views/employees'),
       meta: { title: '员工', icon: 'people' }
+    },
+    {
+      path: 'detail/:id', // params传参 动态路由传参
+      component: () => import('@/views/employees/detail'),
+      hidden: true, // 不在左侧菜单显示
+      meta: {
+        title: '员工详情' // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
+      }
     }
   ]
 }
